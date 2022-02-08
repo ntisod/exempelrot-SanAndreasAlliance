fa85d081f4e449e99a05c9ad6d5f52239f02e18a
SanAndreasAlliance <72020556+SanAndreasAlliance@users.noreply.github.com>
2022-02-08 10:44:44 +0100

    kod

--- Uppgifter/uppgift01.php
+++ Uppgifter/uppgift01.php
@@ -1,11 +1,27 @@
-<?php
-header("Content-type: text/html; charset=utf-8");
-if(empty ($_GET[ " namn "])){
-    echo '<h1>Välkommen! </h1>';
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-else{
-    $namn=filter_input (INPUT_GET, 'namn' );
-    echo "<h1>Väl kommen ($namn}l</h1>";
-    echo "<p Namnet $namn inneháller ", strlen ($namn), " tecken. <p>";
-}
+
+<?php 
+header("Content-type: text/html; charset=utf-8"); 
+if(empty($_GETUnamn1)){
+    echo '<h1>Valkommen!</h1>'; 
+} 
+else{ 
+$namn=filter_input(INPUT_GET, 'namn'); 
+echo "<h1>Valkommen ${namn}!</hl>"; 
+echo "<p>Namnet $namn innehaller ", strlen($namn), " tecken.<p>"; 
+} 
+?>
+
+
+
+</body>
+</html>
